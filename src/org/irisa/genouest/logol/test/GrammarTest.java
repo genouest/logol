a1542beb7a01c1c36a238566d61bfad38852e64e
Olivier Sallou <olivier.sallou@irisa.fr>
2016-04-08 14:13:11 +0200

    fix typo

--- src/org/irisa/genouest/logol/test/GrammarTest.java
+++ src/org/irisa/genouest/logol/test/GrammarTest.java
@@ -2355,7 +2355,7 @@ public class GrammarTest {
 
 		// Skip test in Travis
 		try {
-		 	is_travis = System.getenv('TRAVIS');
+		 	is_travis = System.getenv("TRAVIS");
 		 	assumeTrue(is_travis==null);
 			return;
 		}
